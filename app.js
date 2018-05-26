b7c5d0d3afa9d3ddf7ffabefd26e206c99d3a9fd
Bob Reilley <rpreilley@gmail.com>
2018-05-26 09:13:23 -0500

    added nodemon to server

--- app.js
+++ app.js
@@ -38,4 +38,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(app.get('port'),
+  function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+app.set('port', process.env.PORT || 3000);
+
 module.exports = app;
