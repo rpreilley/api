4d11c21672fe8063acee33c2088784288aeee295
Bob Reilley <rpreilley@gmail.com>
2018-05-27 00:56:19 -0500

    added cors options

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  method: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
