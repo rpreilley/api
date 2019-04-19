bef4a5474b0c259d4c9853c014fbc67b268d98cb
Bob Reilley <rpreilley@gmail.com>
2019-04-18 20:00:56 -0500

    re-structured some responses

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,10 @@ var User = require('../models/user.js');
 /* GET ALL USERS */
 router.get('/', function(req, res, next) {
   User.find(function (err, user) {
-    if (err) return next(err);
-    res.json(user);
+    if (err) return next(err); 
+    res.status(200).json({
+      ret: user
+    });
   });
 });
 
@@ -15,7 +17,9 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   User.findById(req.params.id, function (err, user) {
     if (err) return next(err);
-    res.json(user);
+    res.status(200).json({
+      ret: user
+    });
   });
 });
 
@@ -24,31 +28,43 @@ router.post('/signup', function(req, res, next) {
   console.log(req.body)
   User.create(req.body, function (err, user) {
     if (err) return next(err);
-    res.json(user);
+    res.status(200).json({
+      ret: user
+    });
   });
 });
 
 /* LOGIN USER */
 router.post('/login', function(req, res, next) {
   User.findOne({ email: req.body.email }).exec(function(err, user) {
-    user.comparePassword(req.body.password, function(err, isMatch) {
-      if (isMatch) {
-        req.session.currentUserID = user.id;
-        console.log(req.session);
-        console.log('is match', isMatch);
-        res.json(200,
-          {
-            ret: user,
-            msg: "Login successful."
-          }
-        );
-      } else {
-        res.json(400, { msg: "Invalid email or password. Please try again." });
-      }
-      if (err) {
-        res.json(400, { msg: "There was an issue logging in. Please try again." });
-      }
-    });
+    if (user) {
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch) {
+          req.session.currentUserID = user.id;
+          console.log(req.session);
+          console.log('is match', isMatch);
+          res.json(200,
+            {
+              ret: user,
+              msg: "Login successful."
+            }
+          );
+        } else {
+          res.status(400).json({
+            msg: "Invalid email or password. Please try again."
+          });
+        }
+        if (err) {
+          res.status(400).json({
+            msg: "There was an issue logging in. Please try again."
+          });
+        }
+      });
+    } else {
+      res.status(404).json({
+        msg: "User not found with email proveded."
+      });
+    }
   });
 });
 
@@ -56,7 +72,9 @@ router.post('/login', function(req, res, next) {
 router.put('/:id', function(req, res, next) {
   User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
     if (err) return next(err);
-    res.json(user);
+    res.status(200).json({
+      ret: user
+    });
   });
 });
 
@@ -64,7 +82,9 @@ router.put('/:id', function(req, res, next) {
 router.delete('/:id', function(req, res, next) {
   User.findByIdAndRemove(req.params.id, req.body, function (err, user) {
     if (err) return next(err);
-    res.json(user);
+    res.status(200).json({
+      ret: user
+    });
   });
 });
 
